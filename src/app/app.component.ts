22b12ef731640ede526edb66e3e3b51b94143f49
Tien Nguyen Truong <tien.ngtruong@gmail.com>
2020-12-24 15:35:23 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular11FileUpload';
+  title = 'Angular 11 File Upload';
 }
